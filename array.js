295adcde8169cb433970e6fb4a676e63c5c55e6b
trentduncan <trent.dunc@gmail.com>
2018-05-16 09:34:13 -0700

    added array and answered some of the questions

---
+++ array.js
@@ -0,0 +1,98 @@
+'use strict';
+const Memory = require('./memory');
+const memory = new Memory();
+
+
+class Array {
+  constructor() {
+    this.length = 0;
+    this._capacity = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+    this._capacity = size;
+  }
+
+  retrieve(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+    
+  }
+  pop() {
+    if (this.length === 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.ptr + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+}
+
+// function main(){
+
+//   Array.SIZE_RATIO = 3;
+
+//   //create an instance of the array class
+//   let arr = new Array();
+
+//   //add an item to the array
+//   arr.push(3);
+
+//   console.log(arr);
+// }
+
+// What is the length, capacity and memory address of your array?
+// Array { length: 1, _capacity: 3, ptr: 0 }
+
+// function main(){
+
+//     Array.SIZE_RATIO = 3;
+
+//     //create an instance of the array class
+//     let arr = new Array();
+
+//     //add an item to the array
+//     arr.push(3);
+//     arr.push(5);
+//     arr.push(15);
+//     arr.push(19);
+//     arr.push(45);
+//     arr.push(10);
+
+//     console.log(arr);
+// }
+// main();
+
+// What is the length, capacity and memory address of your array? 
+// Explain the result of your program after adding the new lines of code
+
+// Array { length: 6, _capacity: 12, ptr: 3 }
+// the length is now 6 and since it exceeded the original capacity,
+// the size was increased to 12 due to the size of the array and the 
+// size ratio. The ptr is shifted over to where the previous arrays 
+// capacity ended.
+
+
+
+
+
