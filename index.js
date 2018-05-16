0ea8acfe115b9e9087888c92e92d227e0d77bbb6
trentduncan <trent.dunc@gmail.com>
2018-05-16 12:29:02 -0700

    finished merge ordered lists

--- index.js
+++ index.js
@@ -35,26 +35,90 @@
 
 // O(n)
 
-const array = [4,100,-104,110,2,1];
+// const array = [4,100,-104,110,2,1];
 
-const maxSum = (arr) => {
-  const sumArr = [];
-  for (let i=0; i < arr.length; i++) {
-    if (i === 0) {
-      sumArr.push(arr[i]);
-    } else {
-      sumArr.push(arr[i] + sumArr[i-1]);
-    }
-  }
-  let result = sumArr[0];
-  for (let i=0; i < sumArr.length; i++) {
-    if (sumArr[i] > result) {
-      result = sumArr[i];
-    }
-  }
-  return result;
-};
+// const maxSum = (arr) => {
+//   const sumArr = [];
+//   for (let i=0; i < arr.length; i++) {
+//     if (i === 0) {
+//       sumArr.push(arr[i]);
+//     } else {
+//       sumArr.push(arr[i] + sumArr[i-1]);
+//     }
+//   }
+//   let result = sumArr[0];
+//   for (let i=0; i < sumArr.length; i++) {
+//     if (sumArr[i] > result) {
+//       result = sumArr[i];
+//     }
+//   }
+//   return result;
+// };
 
-console.log(maxSum(array));
+// console.log(maxSum(array));
 
 //  O(2n) => O(n)
+
+// const array1 = [1, 3, 6, 8, 11];
+// const array2 = [2, 3, 5, 8, 9];
+// const mergeArr = (arr1, arr2) => {
+//   const result = [];
+//   for (let i=0; i < arr1.length; i++) {
+//     result.push(arr1[i]);
+//     for (let j=0; j < arr2.length; j++) {
+//       if (arr2[j] === arr1[i]) {
+//         result.push(arr2[j]);
+//       }
+//     }
+//   }
+// };
+
+// const mergeArr = (arr1, arr2) => {
+//   const result = [];
+//   const largerArr = arr1.length >= arr2.length ? arr1.length : arr2.length;
+//   for (let i=0; i < largerArr.length; i++) {
+//     if (arr1[i] > arr2[i]) {
+//       result.push(arr2[i]);
+//     } 
+//     else if (arr1[i] === arr2[i]) {
+//       result.push(arr1[i]);
+//       result.push(arr2[i]);
+//     }
+//     else if (arr1[i] < arr2[i]) {
+//       result.push(arr1[i]);
+//       result.push(arr2[i]);
+//     }
+//   }
+// };
+
+// const mergeArr = (arr1, arr2, res=[], count=0) => {
+//   let min = 0;
+//   if (arr1.length + arr2.length === 0) {
+//     return console.log(res);
+//   }
+//   if (arr1.length === 0) {
+//     min = arr2[0];
+//     arr2.shift();
+//   }
+//   if (arr2.length === 0) {
+//     min = arr1[0];
+//     arr1.shift();
+//   }
+//   if (arr1[0] <= arr2[0]) {
+//     min = arr1[0];
+//     arr1.shift();
+//   }
+//   else if (arr1[0] > arr2[0]) {
+//     min = arr2[0];
+//     arr2.shift();
+//   }
+//   res[count] = min;
+//   count++;
+//   mergeArr(arr1, arr2, res, count);
+// };
+
+// console.log(mergeArr(array1,array2));
+
+// O(n)
+
+
